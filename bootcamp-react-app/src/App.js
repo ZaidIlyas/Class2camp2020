dce42fe6a59d6bbcd4066451316d803b2aa47a51
ZaidIlyas <zeee.ilyas@gmail.com>
2020-06-22 00:24:37 +0500

    Update App.js

--- bootcamp-react-app/src/App.js
+++ bootcamp-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello New react</h1>
+        <h1>Hello React</h1>
       </header>
     </div>
   );
